b27263c786b365cdbcf12a567c17d6d598d5bd14
chengli <chengli@today36524.com.cn>
2020-06-15 17:59:23 +0800

    feat:AOP

---
+++ src/main/java/com/atguigu/config/MainConfigOfAOP.java
@@ -0,0 +1,13 @@
+package com.atguigu.config;
+
+/**
+ * @author ZerlindaLi create at 2020/6/15 17:36
+ * @version 1.0.0
+ * @description MainConfigOfAOP
+ * AOP: 【动态代理】
+ *      指程序运行期间动态的将某段代码切入到指定方法指定位置进行运行的编程方式
+ *
+ * 1. 导入aop模块
+ */
+public class MainConfigOfAOP {
+}
